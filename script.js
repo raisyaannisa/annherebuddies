f2fdc2d772f349bb435212e0b93ee7b37f73b70a
raisyaannisa <98729169+raisyaannisa@users.noreply.github.com>
2022-10-13 16:03:36 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+function clickButton(){
+    const love = document.querySelectorAll('.love');
+    const you = document.querySelectorAll('.you');
+    const more = document.querySelectorAll('.more');
+    const aldean = document.querySelectorAll('.aldean');
+    const loveAnimation = document.querySelectorAll('.love-animation');
+
+    const button = document.querySelector('.btn');
+    const borderDiv = document.querySelector('.border');
+
+    upin.forEach((nodelist) => {
+        nodelist.classList.toggle('transition-out');
+    });
+
+    ipin.forEach((nodelist) => {
+        nodelist.classList.toggle('transition-out');
+    });
+
+    button.classList.toggle('transition-out');
+
+    setTimeout(() => {
+        upin.forEach((nodelist) => {
+            nodelist.classList.toggle('hide');
+            nodelist.classList.toggle('transition-out');
+        });
+    
+        ipin.forEach((nodelist) => {
+            nodelist.classList.toggle('hide');
+            nodelist.classList.toggle('transition-out');
+        });
+
+        button.classList.toggle('hide');
+        borderDiv.classList.toggle('border-t-red-soft');
+        borderDiv.classList.toggle('px-5');
+        borderDiv.classList.toggle('py-2');
+        button.classList.toggle('transition-out');
+
+
+        loveAnimation.forEach((nodelist) => {
+            nodelist.classList.toggle('hide');
+            nodelist.classList.toggle('love');
+        });
+    }, 1500)
+
+    
+}
